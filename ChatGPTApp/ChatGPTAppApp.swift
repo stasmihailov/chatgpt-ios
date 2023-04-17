e3ed1f95296529a1342f0e58b07aa51f164efb52
denisbabochenko <babochenko.denis2@gmail.com>
2023-04-17 16:30:13 +0100

    Initial Commit

---
+++ ChatGPTApp/ChatGPTAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTAppApp.swift
+//  ChatGPTApp
+//
+//  Created by Denis Babochenko on 17/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
