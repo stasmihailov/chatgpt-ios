877eac7bd88f4b4e357efed9522b3864bd660b99
denisbabochenko <babochenko.denis2@gmail.com>
2023-05-05 16:21:37 +0100

    add "drag back" gesture to chat

--- ChatGPTApp/Screens/Chat.swift
+++ ChatGPTApp/Screens/Chat.swift
@@ -85,6 +85,16 @@ struct ExistingChatBody: View {
     }
 }
 
+extension UINavigationController: UIGestureRecognizerDelegate {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return viewControllers.count > 1
+    }
+}
+
 struct Chat: View {
     @EnvironmentObject var keychain: KeychainManagerWrapper
     @EnvironmentObject var api: OpenAIApiWrapper
